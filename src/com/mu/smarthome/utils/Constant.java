5ac2da5a2dfd42253803c5216fbc4cac4eb56320
Mu <Mu@MuWenLei>
2015-10-22 18:06:16 +0800

    d

--- src/com/mu/smarthome/utils/Constant.java
+++ src/com/mu/smarthome/utils/Constant.java
@@ -1,5 +1,7 @@
 package com.mu.smarthome.utils;
 
+import com.mu.smarthome.R;
+
 public class Constant {
 
 	/** 根目�? **/
@@ -11,5 +13,19 @@ public class Constant {
 	public static final boolean LOGOFF_VERBOSE = false;
 	// 是否关闭debug输出
 	public static final boolean LOGOFF_DEBUG = false;
+
+	public static final String TYPE_OUTLET = "01"; // 插座
+
+	public static final String TYPE_AIRCONDITIONING = "02";// 空调
+
+	public static final String TYPE_INFRARED = "03"; // 红外
+
+	public static final String TYPE_COMBINATIONSWITCH = "04";// 复合开关
+
+	public static final String TYPE_GANGEDSWITCH = "05";// 双联开关
+
+	public static final String TYPE_SINGLESWITCH = "06"; // 单联开关
+
+	public static final String TYPE_NONE = "07";// 未知
 
 }
